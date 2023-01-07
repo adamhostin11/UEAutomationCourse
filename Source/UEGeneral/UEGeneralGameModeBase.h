0f309536c6534a416c0750988811740c40eb8b43
Adam Hostin <adamhostin@yahoo.com>
2023-01-07 12:27:52 +0100

    UE setup

---
+++ Source/UEGeneral/UEGeneralGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UEGeneralGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UEGENERAL_API AUEGeneralGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
